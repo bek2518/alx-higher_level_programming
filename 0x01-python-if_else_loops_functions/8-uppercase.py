2a0a8b69cd93b4f6435640ccf37d07a860c3670a
bek2518 <bereketb20@hotmail.com>
2022-09-21 19:51:21 +0300

    Prints string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    len_s = len(str)
+    for i in range(0, len_s):
+        asc = ord(str[i])
+        if asc > 64 and asc < 91:
+            asc = asc
+        elif asc > 96 and asc < 123:
+            asc = asc - 32
+        if i != len_s - 1:
+            print("{:c}".format(asc), end = '')
+        else:
+            print("{:c}".format(asc))
+uppercase("best")
