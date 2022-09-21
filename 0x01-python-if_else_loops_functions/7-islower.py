ef2a19b94a97d3db2ecb71be0db29580f6c345bd
bek2518 <bereketb20@hotmail.com>
2022-09-21 19:22:17 +0300

    RCemoved print line

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,6 @@
 def islower(c):
     c = ord(c)
     if c > 96 and c < 123:
-        print("{:c} is lower".format(c))
         return (True)
-    elif c >66 and c < 91:
-        print("{:c} is upper".format(c))
+    elif c > 66 and c < 91:
         return (False)
