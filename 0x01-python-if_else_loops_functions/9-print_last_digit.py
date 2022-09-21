9f7d47843edc6ed079e23d1e4a5b4581853b1b8b
bek2518 <bereketb20@hotmail.com>
2022-09-21 20:20:33 +0300

    Removed negative multiplier

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number > 0:
         number = number % 10
     elif number <= 0:
-        number = (((number * -1) % 10) * -1)
+        number = ((number * -1) % 10)
     print("{:d}".format(number), end='')
     return (number)
