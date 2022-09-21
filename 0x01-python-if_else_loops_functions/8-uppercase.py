a015736924f11b74e9c1e6f852c7c2443a760a5b
bek2518 <bereketb20@hotmail.com>
2022-09-21 19:59:48 +0300

    Minor edit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,9 @@ def uppercase(str):
     len_s = len(str)
     for i in range(0, len_s):
         asc = ord(str[i])
-        if asc > 64 and asc < 91:
-            asc = asc
-        elif asc > 96 and asc < 123:
+        if asc > 96 and asc < 123:
             asc = asc - 32
+        else
+            asc = asc
             print("{:c}".format(asc), end='')
     print()
