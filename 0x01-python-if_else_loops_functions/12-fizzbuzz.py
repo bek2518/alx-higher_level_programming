4746c20ceeb43710f3ac008fc8622cc30bf139d9
bek2518 <bereketb20@hotmail.com>
2022-09-21 20:34:08 +0300

    Prints numbers from 1 to 10

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        fizz = i % 3
+        buzz = i % 5
+        if fizz == 0 and buzz == 0:
+            print("FizzBuzz ", end='')
+        elif fizz == 0 and buzz != 0:
+            print("Fizz ", end='')
+        elif fizz != 0 and buzz == 0:
+            print("Buzz ", end='')
+        else:
+            print("{:d} ".format(i), end='')
+fizzbuzz()
