46919958d860d9efb339ec92a70a60731747d374
bek2518 <bereketb20@hotmail.com>
2022-09-21 20:14:30 +0300

    Prints the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        number = number % 10
+    elif number <= 0:
+        number = (((number * -1) % 10) * -1)
+    return (number)
