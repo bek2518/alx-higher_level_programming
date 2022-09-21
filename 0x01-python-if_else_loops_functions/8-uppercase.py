bdbdc3c174fc925872a33273431c3d9d3776ef8b
bek2518 <bereketb20@hotmail.com>
2022-09-21 20:05:16 +0300

    Minor edit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,11 @@ def uppercase(str):
     len_s = len(str)
     for i in range(0, len_s):
         asc = ord(str[i])
-        if asc > 96 and asc < 123:
+        if asc >= 97 and asc <= 122:
             asc = asc - 32
         else:
             asc = asc
-            print("{:c}".format(asc), end='')
+        print("{:c}".format(asc), end='')
     print()
+uppercase("z")
+uppercase("Holberton")
