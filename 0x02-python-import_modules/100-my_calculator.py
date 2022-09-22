5bf76d3adcded3aeea964acefece90b6243953a9
bek2518 <bereketb20@hotmail.com>
2022-09-22 20:38:58 +0300

    Minor edit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
     no = len(argv) - 1
     if no != 3:
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif op == '*':
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    elif op == '/'
+    else:
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
