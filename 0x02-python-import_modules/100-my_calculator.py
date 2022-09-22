55345531eca0f2137b42cc9f9ed3deaf8fa5e995
bek2518 <bereketb20@hotmail.com>
2022-09-22 21:27:27 +0300

    Changed to f string

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     no = len(sys.argv) - 1
     if no != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print(f'Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit (1)
     op = sys.argv[2]
     if op != '+' and op != '-' and op != '*' and op != '/':
-        print("Unknown operator. Available operators: +, -, * and /")
+        print(f'Unknown operator. Available operators: +, -, * and /')
         exit (1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
