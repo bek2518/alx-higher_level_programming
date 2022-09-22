c7be33b077f911a9df830a01aaabc208b674ddef
bek2518 <bereketb20@hotmail.com>
2022-09-22 20:43:18 +0300

    Revert

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,14 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    no = len(argv) - 1
+    no = len(sys.argv) - 1
     if no != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        sys.exit (1)
     op = argv[2]
     if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        sys.exit (1)
     a = int(argv[1])
     b = int(argv[3])
     if op == '+':
