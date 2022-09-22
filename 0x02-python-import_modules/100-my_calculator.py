c8c63a1dbdf9234b6cf3450023015208c05c0d62
bek2518 <bereketb20@hotmail.com>
2022-09-22 18:21:30 +0300

    Minor edit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,23 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
+    
     no = len(sys.argv) - 1
     if no != 3:
         print("Usage: ./100-mycalculator.py <a> <operator> <b>")
         exit (1)
+    
     a = int(sys.argv[1])
     operator = ord(sys.argv[2])
     b = int(sys.argv[3])
     if operator == 43:
-        print("{} {:c} {} = {}".format(a, operator, b, add(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == 45:
-        print("{} {:c} {} = {}".format(a, operator, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == 42:
-        print("{} {:c} {} = {}".format(a, operator, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == 47:
-        print("{} {:c} {} = {}".format(a, operator, b, div(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit (1)
