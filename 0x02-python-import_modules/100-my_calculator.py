c69952901df0e6600b135007accf31c73fec1755
bek2518 <bereketb20@hotmail.com>
2022-09-22 18:34:01 +0300

    variable change and minor edit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,22 +5,22 @@ if __name__ == "__main__":
     
     no = len(sys.argv) - 1
     if no != 3:
-        print("Usage: ./100-mycalculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit (1)
     
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, *, and /")
         exit (1)
 
-    if operator == '+':
+    if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
+    elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == '/':
+    else:
         print("{} / {} = {}".format(a, b, div(a, b)))
