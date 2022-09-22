9fff837820661005ed57b12132545ec71e6091e7
bek2518 <bereketb20@hotmail.com>
2022-09-22 18:50:25 +0300

    Minor changes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,26 +2,21 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
     no = len(sys.argv) - 1
     if no != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit (1)
-    
     op = sys.argv[2]
-
     if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit (1)
-    
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-
     if op == '+':
-        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {}".format(a, b, add(a, b)))
     elif op == '-':
-        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {}".format(a, b, sub(a, b)))
     elif op == '*':
-        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {}".format(a, b, mul(a, b)))
     else:
-        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {}".format(a, b, div(a, b)))
