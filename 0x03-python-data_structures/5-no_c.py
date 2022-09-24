b911a9df960b6f3a0b4653b3eb912862b19f0a4a
bek2518 <bereketb20@hotmail.com>
2022-09-24 12:02:06 +0300

    Defined new string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = my_string[:]
     for i in range(len(my_string)):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
             new_string = my_string[:i] + my_string[(i + 1):]
-            return (new_string)
+        return (new_string)
