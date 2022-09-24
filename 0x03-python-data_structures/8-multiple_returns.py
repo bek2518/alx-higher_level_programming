2859b6ff927257e6d95ce98fb00d6d2e9a020787
bek2518 <bereketb20@hotmail.com>
2022-09-24 14:07:31 +0300

    Added if an empty statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_sentence = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        tuple_sentence = (len(sentence), None)
+    else:
+        tuple_sentence = (len(sentence), sentence[0])
     return(tuple_sentence)
