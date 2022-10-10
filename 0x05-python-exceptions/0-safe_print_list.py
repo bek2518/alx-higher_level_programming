dbdf1abc218055285fa4cf948cf75294064d4308
bek2518 <bereketb20@hotmail.com>
2022-10-10 22:17:19 +0300

    changed finally with else

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end='')
         except:
             break
-        finally:
+        else:
             j = j+1
     print("\n")
     return(j)
