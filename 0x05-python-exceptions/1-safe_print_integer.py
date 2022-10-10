ea41e1d32ab6d6f33105ad4375d193d6ffcfd8d3
bek2518 <bereketb20@hotmail.com>
2022-10-10 22:43:13 +0300

    Prints first elements of list only integers

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except IndexError as error:
+    except IndexError in error:
         return (False)
     else:
         return (True)
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            j = j + 1
+    print()
+    return j
