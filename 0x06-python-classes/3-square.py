bb5954b549e4a2986e0ebe5c8c0c536339f4e37d
bek2518 <bereketb20@hotmail.com>
2022-10-11 21:23:34 +0300

    Pycode style edit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,6 +19,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         area_size = self.__size * self.__size
         return area_size
