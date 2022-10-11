3f20b7c79b1aef17d4f73912c2ed16d40ef62244
bek2518 <bereketb20@hotmail.com>
2022-10-11 20:50:05 +0300

    Create a class with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square
+    """
+    def __init__(self, __size):
+        """Initialize method that stores size of square
+        Args:
+            param1 (int): size of square
+        """
+        self.__size = size
