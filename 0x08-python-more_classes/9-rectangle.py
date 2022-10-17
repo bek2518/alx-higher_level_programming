01f85e4bee8350c79158290eaab6c931096f1ba1
bek2518 <bereketb20@hotmail.com>
2022-10-17 12:53:59 +0300

    Returns new rectangle instance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -112,7 +112,7 @@ class Rectangle:
         '''
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         '''
@@ -131,3 +131,14 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
+    
+    @classmethod
+    def square(cls, size=0):
+        '''
+        Returns new rectangle with width == height == size
+
+        Args:
+            cls: new rectangle class
+            size: width and height of rectangle
+        '''
+        return cls(size, size)
