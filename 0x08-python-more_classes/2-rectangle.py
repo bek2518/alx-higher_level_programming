3e4667e5d1118f861e9403b4b35d1e61ac8cfa79
bek2518 <bereketb20@hotmail.com>
2022-10-17 11:31:09 +0300

    Area and perimeter of a rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+'''
+Rectangle class definition
+'''
+
+
+class Rectangle:
+    '''
+    Initiation of Rectangle class
+    '''
+    def __init__(self, width=0, height=0):
+        '''
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        '''
+        self.__width = width
+        self.__height = height
+
+        @property
+        def width(self):
+            '''
+            Retreives width
+            '''
+
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            '''
+            sets width
+
+            Args:
+                value: value to set width to
+            '''
+            if type(value) not int:
+                raise TypeError("width must be an integer")
+            elif value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        @property
+        def height(self):
+            '''
+            Retreives height
+            '''
+
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            '''
+            Sets height
+
+            Args:
+                value: value to set height to
+            '''
+            if type(value) not int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
+        def area(self):
+            '''
+            Calculates area of a rectangle
+            Return:
+                area of the rectangle
+            '''
+            return self.__height * self.__width
+
+        def perimeter(self):
+            '''
+            Calculates the perimeter of a rectangle
+            Return:
+                0 if either width or height is zero
+                else the rectangle perimeter
+            '''
+            if width = 0 or height = 0:
+                return 0
+            else:
+                return (2 * (self.__width + self.__height))
