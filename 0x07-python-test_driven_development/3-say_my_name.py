b361b4f477a24fb8a58f42b8bbbcd622296a0ba1
bek2518 <bereketb20@hotmail.com>
2022-10-21 13:37:46 +0300

    Prints a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+module which prints a given name
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''
+    Prints name provided
+
+    Args:
+        first_name: first name
+        last_name: last name
+    '''
+
+    if not isinstance(first_name, str):
+        raise TypeError("first name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last name must be a string")
+    print("My name is {} {}". format(first_name, last_name))
