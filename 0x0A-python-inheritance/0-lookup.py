54135e839dc6aa8146ffad1a93d326fff541706b
bek2518 <bereketb20@hotmail.com>
2022-10-24 18:55:58 +0300

    Return list of attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def lookup(obj):
+    '''
+    Function that returns the list of available attribues
+    and methods of an object
+
+    Args:
+        obj: object
+    '''
+
+    return dir(obj)
