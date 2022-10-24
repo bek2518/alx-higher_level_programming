1e7fad361cc61aee4f8e8fb79ebcdabb7ba07adb
bek2518 <bereketb20@hotmail.com>
2022-10-24 20:36:31 +0300

    Inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+BaseGeometry class
+'''
+
+
+class BaseGeometry:
+    '''
+    Base class
+    '''
+
+    def area(self):
+        '''
+        Function that raises an exception
+        '''
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''
+        Function that validates value
+
+        Args:
+            name: string name
+            value:value
+        '''
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectange(BaseGeometry):
+    '''
+    Rectangle class that inherits from BaseGeometry
+    '''
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = heigh
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
