6bc5644717027e26da6e582a4a0617cb16fa9ebe
bek2518 <bereketb20@hotmail.com>
2022-10-26 19:27:48 +0300

    Appends new line

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,26 @@
+#!/use/bin/python3
+'''
+append_after
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''
+    Function that inserts a line of text to a file after
+    each line with a specific string
+    
+    Args:
+        filename: filename
+        search_string: string to be searched
+        new_string: string to append
+    '''
+
+    output = ''
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            output = output + line
+            if search_string in line:
+                output = output + new_string
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(output)
