4507532ea88126b37d53454d4016b3df702a38e8
bek2518 <bereketb20@hotmail.com>
2022-10-29 06:10:43 +0300

    Added size setter and getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,20 @@ class Square(Rectangle):
         self.size = size
 
     @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+        self.__size = value
+
+    @property
     def width(self):
         return self.__width
 
@@ -72,8 +86,8 @@ class Square(Rectangle):
             print(' ' * self.x + '#' * self.width)
 
     def __str__(self):
-        return (("[Square] ({:d}) {:d}/{:d} - {:d}/{:d}")
-                .format(self.id, self.x, self.y, self.width, self.height))
+        return (("[Square] ({:d}) {:d}/{:d} - {:d}")
+                .format(self.id, self.x, self.y, self.width or self.height))
 
     def update(self, *args, **kwargs):
         if args:
