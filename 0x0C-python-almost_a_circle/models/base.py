2cf4a25b074d85456c97485f37f6b92b4bc1f95c
bek2518 <bereketb20@hotmail.com>
2022-10-29 06:30:12 +0300

    Return json string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
 Create a Base class
 '''
+import json
 
 
 class Base:
@@ -17,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return ('')
+        else:
+            return json.dumps(list_dictionaries)
