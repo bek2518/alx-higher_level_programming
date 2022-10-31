8a63ad8de43db5aafac56f76102ab77a7bb713a1
bek2518 <bereketb20@hotmail.com>
2022-10-31 21:35:31 +0300

    Documentation edit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,14 +13,47 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''
+        Intitalization
+
+        Args:
+            id: id
+        '''
+
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
     
+    @staticmethod
     def to_json_string(list_dictionaries):
+        '''
+        Static method that changes to a json string
+
+        Args:
+            list_dictionaries: list of dictionary
+        '''
+
         if list_dictionaries is None:
-            return ('')
+            return ([])
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        Class method that saves json representation into a file
+
+        Args:
+            cls: class
+            list_objs: list of objects
+        '''
+
+        try:
+            json_string = cls.to_json_string(
+                [x.to_dictionary() for x in list_objs])
+        except BaseException:
+            json_string = '[]'
+        with open(cls.__name__ + '.json', 'w') as f:
+            f.write(json_string)
