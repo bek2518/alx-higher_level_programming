f8e6aca02c7208b9c34321d487529ca18bfa0adf
bek2518 <bereketb20@hotmail.com>
2022-11-01 20:00:47 +0300

    Trial

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -36,7 +36,7 @@ class Base:
         '''
 
         if list_dictionaries is None:
-            return ([])
+            return ('[]')
         else:
             return json.dumps(list_dictionaries)
 
