f8501acac53337d4732add369ec3ec6b795714a7
bek2518 <bereketb20@hotmail.com>
2022-11-30 15:48:46 +0300

    Changed month

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Displays top 3 cities temp during July and August
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city WHERE month = 'July' OR month = 'August' ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city WHERE month = 7 OR month = 8 ORDER BY avg_temp DESC LIMIT 3;
