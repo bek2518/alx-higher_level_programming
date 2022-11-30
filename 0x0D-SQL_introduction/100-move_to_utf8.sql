f990a92f983949692d484dc373703b2c1547820d
bek2518 <bereketb20@hotmail.com>
2022-11-30 15:22:06 +0300

    Added convert

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Converts database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
