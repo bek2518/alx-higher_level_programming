1bc0d78da39d25f06d81247a49a12a3e24ef42fc
bek2518 <bereketb20@hotmail.com>
2022-11-30 18:18:52 +0300

    Modified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- Creates MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL
+ON *.* 
+TO user_0d_1@localhost;
