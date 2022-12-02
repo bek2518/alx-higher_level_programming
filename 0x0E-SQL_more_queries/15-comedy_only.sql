6e4924dec4c9a9aefe3025082e01b52bb56fef09
bek2518 <bereketb20@hotmail.com>
2022-12-02 16:34:35 +0300

    Rearrange

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,6 +3,6 @@ SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
-ON tv_show_genres.genre.id = tv_genres.id
+ON tv_genres.id = tv_show_genres.genre.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
