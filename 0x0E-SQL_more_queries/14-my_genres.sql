ba56c4f676bf74b7ade56b2a2e6240fb9e564816
bek2518 <bereketb20@hotmail.com>
2022-12-02 16:25:08 +0300

    List generes of show

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,8 @@
 -- Lists all genres
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres INNER JOIN tv_show_genres
+SELECT tv_genres.name FROM tv_genres 
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name ORDER BY number_shows DESC;
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
