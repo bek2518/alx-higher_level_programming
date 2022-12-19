2dcb04cda2399369d474c26f112f09fca063dbc7
bek2518 <bereketb20@hotmail.com>
2022-12-19 15:09:26 +0300

    Checks arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+  console.log('No Argument');
+}
+if (process.argv.length === 3) {
+  console.log('Argument found');
+}
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
