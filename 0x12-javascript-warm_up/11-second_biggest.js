03839b840c9c29affc1749b6a1aee1caa788d872
bek2518 <bereketb20@hotmail.com>
2022-12-19 17:32:58 +0300

    Prints seconf biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,33 +6,28 @@ if (process.argv.length <= 3) {
   let biggest;
   let secondBiggest;
 
-  if (process.argv[2] < process.argv[3]) {
-    biggest = process.argv[3];
-    secondBiggest = process.argv[2];
+  if (Number(process.argv[2]) < Number(process.argv[3])) {
+    biggest = Number(process.argv[3]);
+    secondBiggest = Number(process.argv[2]);
   } else {
-    biggest = process.argv[2];
-    secondBiggest = process.argv[3];
+    biggest = Number(process.argv[2]);
+    secondBiggest = Number(process.argv[3]);
   }
 
   if (process.argv.length > 4){
     for (let i = 4; i < process.argv.length; i++) {
       if (biggest > process.argv[i]) {
-        if (secondBiggest > process.argv[i]) {
+        if (secondBiggest > Number(process.argv[i])) {
           biggest = biggest;
           secondBiggest = secondBiggest;
         } else {
           biggest = biggest;
-          secondBiggest = process.argv[i];
+          secondBiggest = Number(process.argv[i]);
         }
       }
       else {
-        if (secondBiggest > process.argv[i]) {
-          biggest = process.argv[i];
-          secondBiggest = secondBiggest;
-        } else {
-          biggest = process.argv[i];
-          secondBiggest = process.argv[i];
-        }
+        secondBiggest = biggest;
+        biggest = process.argv[i];
       }
     }
   }
