5a9ce89056cc76c53d90f1684db3dfcca31f808f
bek2518 <bereketb20@hotmail.com>
2022-12-20 16:39:20 +0300

    Square that inherits from square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const firstSquare = require('./5-square');
+
+module.exports = class Square extends firstSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
