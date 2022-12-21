d5a915e39593f3bae0f0491df7cfaa1fd97f56a0
bek2518 <bereketb20@hotmail.com>
2022-12-21 17:13:02 +0300

    Select by name

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Script that lists all states from the database
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+        ORDER BY states.id")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
