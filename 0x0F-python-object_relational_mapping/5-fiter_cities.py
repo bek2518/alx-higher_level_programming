dea329faa3c0a2d36041fb1cb5f3680c53b2d3ae
bek2518 <bereketb20@hotmail.com>
2022-12-21 17:52:14 +0300

    Takes state as an argument and list all cities

---
+++ 0x0F-python-object_relational_mapping/5-fiter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+Script that lists all cities of a state
+given as an argument from the database
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    c = db.cursor()
+    c.execute("SELECT cities.name FROM cities JOIN states ON \
+        state_id=states.id WHERE states.name LIKE BINARY (%s) \
+            ORDER BY cities.id", (sys.argv[4],))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
