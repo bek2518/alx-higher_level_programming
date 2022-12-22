30905c0983b10d01c3d17c4deea20f98b0e96174
bek2518 <bereketb20@hotmail.com>
2022-12-22 18:00:44 +0300

    Prints all cities from database

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Contains the class definition of City that inherits from Base
+'''
+from model_state import State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    '''
+    Creates City class that inherits from Base
+    '''
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
