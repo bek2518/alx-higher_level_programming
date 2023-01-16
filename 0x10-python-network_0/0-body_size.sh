6be93c816a1803025bf8365fda046fcab4e9cab5
bek2518 <bereketb20@hotmail.com>
2023-01-16 18:27:12 +0300

    Line cut to 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# Bash script that takes in URL, sends request to that URL and
-# displays the size of bosy of the response
-
+# Bash script that takes in URL, sends request to that URL and displays the size of bosy of the response
 curl -sI "$1" | grep Content-Length | cut -d ' ' -f2
