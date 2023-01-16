15e996312131c4816f8ccd0f664659d8c3740bae
bek2518 <bereketb20@hotmail.com>
2023-01-16 19:16:03 +0300

    Unnecessary quotes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash scipt takes in URL, sends POST request to URL and displays the body
-curl -sX "POST" -d "email='test@gmail.com'" -d "subject='I will always be here for PLD'" "$1"
+curl -sX "POST" -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
