5352232fa4f8d16e9bd8a92b05dac00cce24edff
bek2518 <bereketb20@hotmail.com>
2023-01-16 18:28:05 +0300

    bin/bash

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Bash script that takes in URL, sends request to that URL and displays the size of bosy of the response
 curl -sI "$1" | grep Content-Length | cut -d ' ' -f2
