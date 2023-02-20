571f51731e62f223d34fbc3a55a4790c91bfb7a6
bek2518 <bereketb20@hotmail.com>
2023-02-20 18:44:42 +0300

    Mandatory tasks with semistandard edit

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, function (data) {
+  for (let i = 0; i < data.results.length; i++) {
+    $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
+});
